d941ecd7e8c5da7596c92390b77f4a031af7b4b3
krrish143969 <krrishk2006@gmail.com>
2026-05-11 13:58:34 +0530

    updated

--- classesnmethods.py
+++ classesnmethods.py
@@ -2,4 +2,15 @@ class product:
     def __init__(self,productname,price):
         self.proctname = productname
         self.price = price
-        
+    def discount(self,discountvalue):
+        discountamount = (self.price/100)*discountvalue
+        final_price = self.price - discountamount
+        return int(final_price)
+
+
+laptop = product('laptop',55000)
+mobile = product('mobile', 40000)
+print(laptop.discount(25))
+print(mobile.price)
+print(mobile.discount(10))
+
